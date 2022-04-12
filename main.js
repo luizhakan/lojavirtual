2ed4a721240b228839e531b93139ab434520f9cc
luizhakan <uefibiosxx@gmail.com>
2022-04-11 23:31:34 -0300

    shop

---
+++ main.js
@@ -0,0 +1,11 @@
+let brasil = document.getElementById('brasil');
+let colombia = document.getElementById('colombia');
+let argentina = document.getElementById('argentina');
+
+let carrinho = document.getElementById('carrinho');
+
+function adicionarCarrinho(){
+    
+    carrinho.innerHTML += '<p> Brasil</p>'
+}
+
